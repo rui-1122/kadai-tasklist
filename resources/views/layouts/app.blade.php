5d4f37c7ea7c7d04f45b66c20725a770205c4dc2
rui.nishiwaki <rui.n.blink@gmail.com>
2021-07-24 06:52:25 +0000

    validation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,9 @@
         @include('commons.navbar')
 
         <div class="container">
+            {{-- エラーメッセージ --}}
+            @include('commons.error_tasks')
+
             @yield('content')
         </div>
 
